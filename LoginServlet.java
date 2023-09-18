acd92a3a8a9d996831975caefc4b97ae3495fb8a
miausw <143202753+miausw@users.noreply.github.com>
2023-09-18 11:24:23 -0400

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,23 @@
+package com.in28minutes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		writer.println("Dummy Stuff");
+	}
+	
+}
